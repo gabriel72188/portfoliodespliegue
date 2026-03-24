9aac28ebd5c3a43b72e7b8c54ce17d55dd147487
gabriel72188 <gabri72188340@gmail.com>
2026-03-24 23:40:38 +0100

    a

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -120,7 +120,7 @@ const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
               </div>
               
               <Button type="submit" className="w-full bg-primary hover:bg-primary/90">
-                Enviar mensaje
+                Enviar mensaje 1
               </Button>
             </form>
           </Card> 
